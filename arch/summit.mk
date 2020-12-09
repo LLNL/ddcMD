a5de66e3e0397ab47df2d4ebc94038db8b0eb197
Zhang, Xiaohua <zhang30@llnl.gov>
2020-12-09 09:10:10 +0800

    Make config look up current MPI library paths for summit

--- arch/summit.mk
+++ arch/summit.mk
@@ -15,8 +15,10 @@ CPP = mpixlC -std=c++11 -qlanglvl=extended0x
 FC = mpixlf
 
 CPPL = $(shell dirname `which xlc++`)/../lib/
+MPI_INC = $(shell dirname `which mpixlC`)/../include/
+MPI_LIB = $(shell dirname `which mpixlC`)/../lib/
 #CPPL = /usr/tce/packages/xl/xl-beta-2018.04.06/lib/
-CPPL = /sw/summit/xl/16.1.1-4/xlC/16.1.1/lib/
+#CPPL = /sw/summit/xl/16.1.1-4/xlC/16.1.1/lib/
 
 # FFT library needs to be included here
 #-DUSE_FFTW3 
@@ -25,8 +27,8 @@ CPPL = /sw/summit/xl/16.1.1-4/xlC/16.1.1/lib/
 CFLAGS_BASE =  -DWITH_MPI -DWITH_PIO -D_GNU_SOURCE -Wall -Wextra -Wno-unused-parameter \
               -Wno-unknown-pragmas
 #GNU Compiler
-FFTW_DIR=/autofs/nccs-svm1_sw/summit/.swci/1-compute/opt/spack/20180914/linux-rhel7-ppc64le/xl-16.1.1-4/fftw-3.3.8-47bi2nrryanhwf33hk2lsqdkrikoqkk2/lib/
-LDFLAGS_BASE =  -lstdc++ -lm -lc $(FFTW_DIR)/libfftw3.a -L $(CPPL)
+FFTW_DIR=/autofs/nccs-svm1_sw/summit/.swci/1-compute/opt/spack/20180914/linux-rhel7-ppc64le/xl-16.1.1-5/fftw-3.3.8-azzdjlzx2j6dpqvzdir2nwvxypohyfq4/lib/
+LDFLAGS_BASE =  -lstdc++ -lm -lc $(FFTW_DIR)/libfftw3.a -L $(CPPL) -L$(MPI_LIB)
 #XL Compiler
 #LDFLAGS_BASE =  -libmc++ -lstdc++ -lm -lc /usr/tcetmp/packages/fftw/fftw-3.3.5-xl-15.1.5/lib/libfftw3.a -L $(CPPL)
 
@@ -51,7 +53,7 @@ ifeq ($(USE_GPU),1)
   CFLAGS_BASE +=  -DUSE_GPU=1
   LDFLAGS_BASE += -L $(CUDAHOME)/lib64 -lcudart -lcuda -lcurand -lnvToolsExt -lnvrtc -Wl,-rpath,$(CUDAHOME)/lib64
 
-  NVCCFLAGS_BASE += -DUSE_GPU=1 -I ../cub --gpu-architecture=sm_70 -I/usr/local/tools/mvapich-gnu/include -I/autofs/nccs-svm1_sw/summit/.swci/1-compute/opt/spack/20180914/linux-rhel7-ppc64le/xl-16.1.1-4/spectrum-mpi-10.3.0.1-20190611-o4gg2apgfflunavcrlh2izdrrclyynlu/include
+  NVCCFLAGS_BASE += -DUSE_GPU=1 -I ../cub --gpu-architecture=sm_70 -I/usr/local/tools/mvapich-gnu/include -I$(MPI_INC)
   NVCC = $(CUDAHOME)/bin/nvcc  -std=c++11
 #    NVCCFLAGS += --gpu-architecture=sm_60 -I/usr/local/tools/mvapich-gnu/include -I /usr/tce/packages/spectrum-mpi/ibm/spectrum-mpi-2018.02.05/include/
 endif
